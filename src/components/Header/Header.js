e64c163d003cf426f71b0f5b1edf41de091dda6d
Marcelo Glacial <glacial@gmail.com>
2020-03-03 19:23:44 -0500

    header info

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,16 +5,23 @@ const Header = props => {
   return (
     <header className='header'>
       <div className='header__container container'>
-        <div className='header__info'>
-          <div className='header__howto'>Como Funciona</div>
-          <div className='header__contact'>
-            0800 123 2222 Envie mensagem ou ligue
+        <div className='header__info flex-center'>
+          <div className='header__howto flex-center'>
+            <i className='fas fa-info' />
+            Como Funciona
+          </div>
+          <div className='header__contact flex-center'>
+            <i className='fab fa-whatsapp' />
+            <div className='header__number'>
+              <p className='header__tel'>0800 123 2222</p>
+              <p className='header__send'>Envie mensagem ou ligue</p>
+            </div>
           </div>
         </div>
-        <div className='header__logo'>
+        <div className='header__logo  flex-center'>
           <h1 className='header__title'>Quero Bolsa</h1>
         </div>
-        <div className='header__userinfo'>Nome Sobrenome</div>
+        <div className='header__userinfo  flex-center'>Nome Sobrenome</div>
       </div>
     </header>
   );
