019880da9295a3891f5ad6b7417f6d5c44a08ed2
Marcelo Glacial <glacial@gmail.com>
2020-03-04 07:46:47 -0500

    remove card grid

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CardGrid from '../CardGrid/CardGrid';
 import BreadCrumbs from '../BreadCrumbs/BreadCrumbs';
 
 const Main = props => {
@@ -7,11 +6,6 @@ const Main = props => {
     <>
       <main className='main container'>
         <BreadCrumbs />
-        <section className='page__description'>
-          <h2>Bolsas Favoritas</h2>
-          <p>Adicione bolsas</p>
-        </section>
-        <CardGrid />
       </main>
     </>
   );
