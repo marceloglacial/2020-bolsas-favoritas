6c1dbc8992ef6b6b6f3c2f94da33afa580ba298f
Marcelo Glacial <glacial@gmail.com>
2020-03-04 00:20:29 -0500

    add breadcrumbs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CardGrid from '../CardGrid/CardGrid';
+import BreadCrumbs from '../BreadCrumbs/BreadCrumbs';
 
 const Main = props => {
   return (
     <>
       <main className='main container'>
+        <BreadCrumbs />
         <section className='page__description'>
           <h2>Bolsas Favoritas</h2>
           <p>Adicione bolsas</p>
