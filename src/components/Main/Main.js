6e3080403572ec0b7ff15362a664708714a4f498
Marcelo Glacial <glacial@gmail.com>
2020-03-06 13:16:13 -0500

    add content header

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import BreadCrumbs from '../BreadCrumbs/BreadCrumbs';
 import CardGrid from '../CardGrid/CardGrid';
 import './Main.scss';
+import ContentHeader from '../ContentHeader/ContentHeader';
 
 const Main = props => {
   return (
     <>
       <main className='main'>
         <BreadCrumbs />
+        <ContentHeader />
         <CardGrid />
       </main>
     </>
